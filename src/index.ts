207036cf00d413de6d0f18654807e2afbce34369
Yisus <jesusgameroom@gmail.com>
2023-01-05 01:13:37 -0400

    Removed default menu

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import { app, BrowserWindow, dialog } from 'electron';
+import { app, BrowserWindow, Menu } from 'electron';
 import path from 'path';
 
 const publicPath = path.join(__dirname, 'public');
 
 let main : BrowserWindow;
 app.on('ready', () => {
-
     main = new BrowserWindow({
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         },
-        title: 'File Switch - Loading...',
+        title: 'File Switch',
         show: false
     });
+    Menu.setApplicationMenu(null);
     main.loadFile(path.join(publicPath, 'index.html'));
     main.on('ready-to-show', () => {
         main.show();
