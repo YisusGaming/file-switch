023e11ac4081e0c246cd15b72b8e7a99283e06fd
Yisus <jesusgameroom@gmail.com>
2023-01-05 00:51:57 -0400

    Electron app setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hi mom!");
+import { app, BrowserWindow, dialog } from 'electron';
+import path from 'path';
+
+const publicPath = path.join(__dirname, 'public');
+
+let main : BrowserWindow;
+app.on('ready', () => {
+
+    main = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        },
+        title: 'File Switch - Loading...',
+        show: false
+    });
+    main.loadFile(path.join(publicPath, 'index.html'));
+    main.on('ready-to-show', () => {
+        main.show();
+    });
+});
